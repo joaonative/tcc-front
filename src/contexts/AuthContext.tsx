929efcdb0330bce1a40281561f0b8686335fb000
joaonative <jestudos127@gmail.com>
2024-03-14 22:49:11 -0300

    feat: authentication with google & context/provider

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,99 @@
+import { TokenResponse, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+interface AuthContextType {
+  userData: userData | undefined;
+  login: () => void;
+  logout: () => void;
+  isLoggedIn: boolean;
+}
+
+interface userData {
+  sub: string;
+  name: string;
+  given_name: string;
+  picture: string;
+  email: string;
+  email_verified: boolean;
+  locale: string;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [userData, setUserData] = useState<userData>();
+  const [storedData] = useState<userData | undefined>(() => {
+    const stored = localStorage.getItem("user-data");
+    if (stored) {
+      return JSON.parse(stored);
+    }
+    return undefined;
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
+    if (storedData) {
+      return true;
+    }
+
+    return false;
+  });
+
+  const loginCallback = async (tokenResponse: TokenResponse) => {
+    const expirationDate = new Date(
+      Date.now() + tokenResponse.expires_in * 1000
+    );
+
+    document.cookie = `access-token=${
+      tokenResponse.access_token
+    }; expires=${expirationDate.toUTCString()}; path=/; HttpOnly;`;
+
+    console.log(document.cookie);
+
+    const response = await axios.get(
+      `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${tokenResponse.access_token}`
+    );
+
+    setUserData(response.data);
+    localStorage.setItem("user-data", JSON.stringify(response.data));
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    if (storedData) {
+      setUserData(storedData);
+    }
+  }, []);
+
+  const login = useGoogleLogin({
+    onSuccess: loginCallback,
+  });
+
+  const logout = () => {
+    setUserData(undefined);
+    localStorage.removeItem("user-data");
+    document.cookie =
+      "access-token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ userData, login, logout, isLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -11,25 +12,66 @@ import NotFound from "./components/NotFound.tsx";
 import EventSinglePage from "./pages/EventSinglePage.tsx";
 import CommunitiesPage from "./pages/CommunitiesPage.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
+import LoginPage from "./pages/LoginPage";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import SecureRoute from "./SecureRoute.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <Header />
-        <main className="pb-16 lg:pb-24 lg:px-12 md:px-8 px-4">
-          <Routes>
-            <Route element={<App />} path="/" />
-            <Route element={<EventsPage />} path="/eventos" />
-            <Route element={<EventSinglePage />} path={`/evento/:id`} />
-            <Route element={<CommunitiesPage />} path="/comunidades" />
-            <Route element={<App />} path="/sobre" />
-            <Route element={<App />} path="/login" />
-            <Route element={<NotFound />} path="/*" />
-          </Routes>
-        </main>
-      </BrowserRouter>
-      <Footer />
-    </ThemeProvider>
-  </React.StrictMode>
+  <GoogleOAuthProvider
+    clientId={
+      "449306918566-08e7ln3lkq2jj9ulb8pgb4bs5525ebdq.apps.googleusercontent.com"
+    }
+  >
+    <React.StrictMode>
+      <AuthProvider>
+        <ThemeProvider>
+          <BrowserRouter>
+            <Header />
+            <main className="pb-16 lg:pb-24 lg:px-12 md:px-8 px-4">
+              <Routes>
+                <Route element={<App />} path="/" />
+                <Route
+                  element={
+                    <SecureRoute redirectPath="/">
+                      <EventsPage />
+                    </SecureRoute>
+                  }
+                  path="/eventos"
+                />
+                <Route
+                  element={
+                    <SecureRoute redirectPath="/">
+                      <EventSinglePage />
+                    </SecureRoute>
+                  }
+                  path="/evento/:id"
+                />
+                <Route
+                  element={
+                    <SecureRoute redirectPath="/">
+                      <CommunitiesPage />
+                    </SecureRoute>
+                  }
+                  path="/comunidades"
+                />
+                <Route
+                  element={
+                    <SecureRoute redirectPath="/">
+                      <ProfilePage />
+                    </SecureRoute>
+                  }
+                  path="/perfil"
+                />
+                <Route element={<App />} path="/sobre" />
+                <Route element={<LoginPage />} path="/entre" />
+                <Route element={<NotFound />} path="/*" />
+              </Routes>
+            </main>
+          </BrowserRouter>
+          <Footer />
+        </ThemeProvider>
+      </AuthProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
