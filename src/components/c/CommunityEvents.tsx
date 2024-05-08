537dde30bb82484c2d5ce3d683952cb748829fc4
joaonative <jestudos127@gmail.com>
2024-05-08 15:48:10 -0300

    feat: events from community component

---
+++ src/components/c/CommunityEvents.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import Event from "../../interfaces/Event";
+import Button from "../Button";
+import Page from "../Page";
+import EventList from "../e/EventList";
+import { useTheme } from "../../contexts/Theme.context";
+import CreateEventForm from "../e/CreateEventForm";
+
+interface Props {
+  events: Event[];
+}
+
+const CommunityEvents = ({ events }: Props) => {
+  const { darkMode } = useTheme();
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  return (
+    <>
+      <Page>
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl lg:text-3xl font-prompt">
+            {events && events.length >= 1
+              ? "Eventos Ativos"
+              : "Sem eventos, começe agora!"}
+          </h1>
+          <Button
+            variant="primary"
+            onClick={handleOpen}
+            classes="hidden lg:block"
+          >
+            Criar evento
+          </Button>
+        </div>
+        {events && events.length >= 1 ? (
+          <EventList events={events} />
+        ) : (
+          <div className="flex items-center justify-center">
+            <img
+              src={darkMode ? "/notfoundDark.svg" : "/notfound.svg"}
+              width={768}
+              height={512}
+              className="object-cover w-full lg:w-[512px]"
+            />
+          </div>
+        )}
+      </Page>
+      {isOpen && <CreateEventForm handleCancel={() => setIsOpen(false)} />}
+      {!isOpen && (
+        <Button
+          variant="primary"
+          onClick={handleOpen}
+          classes="lg:hidden fixed bottom-24 right-4"
+        >
+          Criar evento
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default CommunityEvents;
